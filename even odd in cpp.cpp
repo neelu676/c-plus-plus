6610988c8cefa7a08d1892fb99589f1046b673bf
neelima_chamanchula <neelima24c@gmail.com>
2025-07-31 11:15:58 +0530

    Add files via upload

---
+++ even odd in cpp.cpp
@@ -0,0 +1,14 @@
+ // even odd in cpp
+ #include<iostream>
+ using namespace std;
+ int main()
+ {
+ 	int number; 
+ 	cout<<"enter number";
+	 cin>>number;
+ 
+ 	if(number%2==0)
+ 	cout<< number<<"even";
+ 	else
+ 	cout<<number<<"odd";
+ }
