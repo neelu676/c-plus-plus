bf172631695bd858a00b490a34eb39cdfec8ea77
neelima_chamanchula <neelima24c@gmail.com>
2025-07-31 11:16:26 +0530

    Add files via upload

---
+++ the given number positive or negative in cpp.cpp
@@ -0,0 +1,14 @@
+ // the given number positive or negative in cpp
+ #include<iostream>
+ using namespace std;
+ int main()
+ {
+ 	int n=0;
+ 
+ if(n>0)
+ cout<<"positive";
+ else if(n<0)
+ cout<<"negative";
+ else
+ cout<<"zero";
+}
